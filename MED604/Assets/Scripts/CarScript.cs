3d2e9a7d7a0fe91c55ff36a01cf9953cb106d18c
totalfreak <dgkartin@live.dk>
2019-04-10 13:52:28 +0200

    Added Car script

--- MED604/Assets/Scripts/CarScript.cs
+++ MED604/Assets/Scripts/CarScript.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class CarScript : MonoBehaviour {
 
+    Rigidbody rb;
+
 	// Use this for initialization
 	void Start () {
-		
+        rb = GetComponent<Rigidbody>();
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        rb.velocity = new Vector3(500 * Time.deltaTime, 0, 0);
+    }
 }
