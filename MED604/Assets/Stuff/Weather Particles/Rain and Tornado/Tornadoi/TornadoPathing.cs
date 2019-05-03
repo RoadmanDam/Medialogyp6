7971a96f1951cd95e1ed5e863eab880337941098
totalfreak <dgkartin@live.dk>
2019-05-03 16:11:29 +0200

    Tornado stuff

--- MED604/Assets/Stuff/Weather Particles/Rain and Tornado/Tornadoi/TornadoPathing.cs
+++ MED604/Assets/Stuff/Weather Particles/Rain and Tornado/Tornadoi/TornadoPathing.cs
@@ -4,47 +4,20 @@ using UnityEngine;
 
 public class TornadoPathing : MonoBehaviour {
 
-    public Transform p1, p2;
-    float speed;
-    bool onFirstPoint;
-    bool onSecondPoint;
+    public Transform point;
+    public float speed = 0.1f;
 
 
     void Start()
     {
-        onFirstPoint = true;
-        onSecondPoint = false;
+        Vector3 globalPos = point.transform.position;
+        point.transform.parent = null;
+        point.transform.position = globalPos;
     }
 
     void Update()
     {
-        Patrol();
-    }
-
-    void Patrol(){
-        speed = 0.1f * Time.deltaTime;
-
-        if (onSecondPoint == false)
-        {
-            transform.position = Vector3.Lerp(transform.position, p2.position, speed);
-        }
-
-        if (Vector3.Distance(transform.position, p2.position) < 1f)
-        {
-            onSecondPoint = true;
-            onFirstPoint = false;
-        }
-
-        if (Vector3.Distance(transform.position, p1.position) < 1f)
-        {
-            onFirstPoint = true;
-            onSecondPoint = false;
-        }
-
-        if (onFirstPoint == false)
-        {
-            transform.position = Vector3.Lerp(transform.position, p1.position, speed);
-        }
+        this.transform.position = Vector3.Lerp(this.transform.position, point.position, speed * Time.deltaTime);
     }
 
 }
